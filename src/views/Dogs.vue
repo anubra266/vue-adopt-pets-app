3638c86a9a518a2eb2d50b027a35c2d0100eefab
anubra266 <anubra266@gmail.com>
2020-11-18 04:07:58 +0100

    Specify pet species

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -3,7 +3,7 @@
     <h1>Dogs for Adoption</h1>
     <b-table striped hover :items="dogs">
       <template #cell(name)="data">
-        <router-link :to="`/pets/${data.index}`">{{ data.value }}</router-link>
+        <router-link :to="`/pets/dogs/${data.index}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
